6ad59455b7c7c9fd72aeb33fba584ab5f7e3ce95
Yibo Wei <david_wyb2001@outlook.com>
2023-05-11 06:11:17 -0700

    Refactored compressor

--- llm_compressor.py
+++ llm_compressor.py
@@ -1,40 +1,42 @@
-import openai
-import json
 from typing import List
+from api import Message, send_messages
+
+# TODO: Use tiktoken to count the tokenized text length
 
 SYSTEM_PROMPT = """
-You will be given a long text in triple single quotes.
-- User will specify the compression ratio and maximum word count. Try to compress the text to the specified ratio without exceeding the maximum word count.
+You will be given a long text in triple single quotes. Think about this step by step:
+- User will specify the compression ratio and maximum word count.
+- Generate a TL;DR version of the text that is compressed to the specified compression ratio and 
+maximum word count.
 - The compressed text should maintain the original meaning, tense, tone, and structure.
 - The compressed text should be one paragraph.
+- Directly output the compressed text in triple single quotes.
+Example:
+User:
+Compression ratio: 1/4
+Maximum word count: 150
+Text: '''Example text'''
+Assistant:
+'''Example compressed text'''
 """
 
-class Message:
-    def __init__(self, role, content):
-        self.role = role
-        self.content = content
-    
-    def to_dict(self):
-        return {
-            "role": self.role,
-            "content": self.content
-        }
-
-def send_messages(messages: List[Message]) -> Message:
-    try:
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[message.to_dict() for message in messages],
-            temperature=0.1
-        )
-        response_message = Message(completion['choices'][0]['message']['role'],completion['choices'][0]['message']['content'])
-        return response_message
-
-    except Exception as e:
-        print("Error: ", e)
-        return None
-
 def compress(text, compression_ratio: str = "1/4", max_words: int = "150"):
+    # Divide text into chunks if it is longer than 2000 words
+    if len(text.split(" ")) >= 2000:
+        chunks = []
+        current_chunk = ""
+        for sentence in text.split(". "):
+            if len(current_chunk.split(" ")) + len(sentence.split(" ")) > 2000:
+                chunks.append(current_chunk)
+                current_chunk = ""
+            current_chunk += sentence + ". "
+        chunks.append(current_chunk)
+        # Compress each chunk
+        compressed_chunks = []
+        for chunk in chunks:
+            compressed_chunks.append(compress(chunk, compression_ratio, max_words))
+        # Compress all chunks into one
+        return compress("\n".join(compressed_chunks), compression_ratio, max_words)
     system_messages = [Message("system", SYSTEM_PROMPT)]
     user_prompt = ""
     user_prompt += f"Compression ratio: {compression_ratio}\n"
@@ -45,4 +47,7 @@ def compress(text, compression_ratio: str = "1/4", max_words: int = "150"):
     ]
     messages = system_messages + user_messages
     response_message = send_messages(messages)
-    return response_message.content
+    if "'''" not in response_message.content:
+        return response_message.content
+    compressed_text = response_message.content.split("'''")[1]
+    return compressed_text
