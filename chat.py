186b57b02a95a8361a38227fbaac4ddc819131d5
Yibo Wei <david_wyb2001@outlook.com>
2023-05-11 10:09:39 -0700

    Merge app_cli.py to chat.py and add clipboard mode

--- chat.py
+++ chat.py
@@ -1,4 +1,7 @@
 import json
+import clipboard
+import argparse
+import os
 from pdf_parser import ContextNode
 from api import Message, send_messages
 
@@ -17,8 +20,9 @@ to be answered with knowledge from the document.
 you need to request for more details with in the following format:
     {"response_type": "request", "targets": [<node_ids>], "original": true/false}
 where <node_ids> are a list of the node_ids in the JSON string. "original" is True if you need the
-original version, False if you need the summarized version. You can request for AT MOST 3 node_ids
-at a time. Pay close attention to the keywords in the question to determine which node_ids to request.
+original version, False if you need the summarized version. You should request for the original
+text if answer is not found in the summarized version.
+Pay close attention to the keywords in the question to determine which node_ids to request.
 6. Once you have the information you need, output the answer in the following format:
     {"response_type": "answer", "content": "<answer>", "references": ["<node_id>", ...]},
 where <answer> is the answer to the question, and references is a list of any node_id that you used to answer the question.
@@ -31,7 +35,7 @@ the full context, then try requesting a different node_id.
 Example:
 Context: {...}
 Question: What is the thread model introduced in the paper?
-Assistant: {"response_type": "request", "content": "2", "original": true}
+Assistant: {"response_type": "request", "targets": ["2"], "original": true}
 
 Example:
 Context: {...}
@@ -40,12 +44,12 @@ Assistant: {"response_type": "answer", "content": "The thread model introduced i
 """
 
 class ContextChatBot:
-    def __init__(self, root_node: ContextNode, curr_question: str = "", print_mode: bool = False):
+    def __init__(self, root_node: ContextNode, curr_question: str = "", clipboard_mode: bool = False):
         self.root_node = root_node
         self.history = [Message("system", SYSTEM_PROMPT)]
         self.curr_question = curr_question
         self.current_contexts = str(self.root_node.get_context(1))
-        self.print_mode = print_mode
+        self.clipboard_mode = clipboard_mode
 
     def load_contexts(self, file_path: str):
         with open(file_path, "r") as f:
@@ -63,13 +67,21 @@ class ContextChatBot:
         if contexts == "":
             contexts = self.current_contexts
         user_message = self.prepare_user_message(question, contexts)
+
         print(user_message.content)
-        input("Press enter to continue...")
+        if self.clipboard_mode:
+            print("The message has been copied to your clipboard")
+            input("Press enter to continue")
+            clipboard.copy(user_message.content)
+            response_content = input("Please paste the AI response:\n")
+        else:
+            input("Press enter to continue")
+            response_message = send_messages(self.history + [user_message])
+            self.history += [Message("user", f"Question: {question}\n")] + [response_message]
+            response_content = response_message.content
 
-        response_message = send_messages(self.history + [user_message])
-        self.history += [Message("user", f"Question: {question}\n")] + [response_message]
+        return self.process_response(response_content)
 
-        return self.process_response(response_message.content)
 
     def prepare_user_message(self, question: str, contexts: str):
         question_prompt = f"Question: {question}\n"
@@ -135,3 +147,50 @@ class ContextChatBot:
         answer = response['content']
         references = response.get('references', [])
         return answer, references
+
+def main():
+    parser = argparse.ArgumentParser(description="Interact with ContextChatBot")
+    parser.add_argument('--read-json', '-r', type=str, required=True, help="Path to the JSON file with context data")
+    parser.add_argument('--clipboard-mode', '-c', action='store_true', help="Enable clipboard mode")
+
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.read_json):
+        print(f"Error: File {args.read_json} does not exist.")
+        return
+
+    if not is_valid_json(args.read_json):
+        print(f"Error: {args.read_json} is not a valid JSON file.")
+        return
+
+    with open(args.read_json, "r") as f:
+        json_string = f.read()
+    
+    root_node = ContextNode.from_json(json_string)
+    chatbot = ContextChatBot(root_node, clipboard_mode=args.clipboard_mode)
+
+    print("Type 'exit' to quit the application.")
+    print(f"Clipboard mode: {args.clipboard_mode}")
+    if args.clipboard_mode:
+        print("Clipboard mode enabled. The AI response will be copied to your clipboard.")
+        clipboard.copy(SYSTEM_PROMPT)
+        input("Now, paste the first system prompt into the chatbot. Press enter to continue.")
+    while True:
+        question = input("Enter your question\n> ")
+        if question.lower() == 'exit':
+            break
+        answer, references = chatbot.ask(question)
+        print(f"Assistant\n> {answer}\n")
+        if references:
+            print(f"References: {references}\n")
+
+def is_valid_json(file_path: str) -> bool:
+    try:
+        with open(file_path, "r") as f:
+            json.load(f)
+        return True
+    except Exception as e:
+        return False
+
+if __name__ == "__main__":
+    main()
