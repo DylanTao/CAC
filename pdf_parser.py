fcb885796df4b79dbb5b021124a8af98babda3f6
Yibo Wei <david_wyb2001@outlook.com>
2023-05-11 06:13:40 -0700

    Refactored pdf_parser

--- pdf_parser.py
+++ pdf_parser.py
@@ -4,17 +4,13 @@ import argparse
 from tqdm import tqdm
 from llm_compressor import compress
 
-class DocumentNode:
-    """
-    A node of a paper
-    """
-    def __init__(self, node_id: str, title: str = "", content: str = "", summary: str = "", word_limit: int = 2000):
+class ContextNode:
+    def __init__(self, node_id: str, title: str = "", content: str = "", summary: str = ""):
         self.node_id = node_id
         self.title = title
         self.content = content
         self.children = []
         self.summary = summary
-        self.word_limit = word_limit
 
     def add_child(self, child):
         self.children.append(child)
@@ -26,7 +22,6 @@ class DocumentNode:
             "content": self.content,
             "summary": self.summary,
             "children": [child.to_dict() for child in self.children],
-            "word_limit": self.word_limit
         }
 
     def to_json(self, indent=None):
@@ -39,7 +34,6 @@ class DocumentNode:
             title=data.get("title", ""),
             content=data.get("content", ""),
             summary=data.get("summary", ""),
-            word_limit=data.get("word_limit", 2000)
         )
         for child_data in data.get("children", []):
             child = cls.from_dict(child_data)
@@ -54,32 +48,6 @@ class DocumentNode:
         data = json.loads(json_string)
         return cls.from_dict(data)
     
-    def apply_word_limit(self):
-        """
-        Apply the word limit to the content of the node and its children
-        If the content of the node is longer than the word limit, the content will be split into chunks
-        Chunks are ending with complete sentences, and appended to the children of the node
-        """
-        if len(self.children) > 0:
-            for child in self.children:
-                child.apply_word_limit()
-        if len(self.content.split(" ")) <= self.word_limit:
-            return
-        # Split the content into sentences
-        sentences = self.content.split(". ")
-        words_in_sentences = [len(sentence.split(" ")) for sentence in sentences]
-        chunks = []
-        current_chunk = ""
-        for i, sentence in enumerate(sentences):
-            if len(current_chunk.split(" ")) + words_in_sentences[i] > self.word_limit:
-                chunks.append(current_chunk)
-                current_chunk = ""
-            current_chunk += sentence + ". "
-        chunks.append(current_chunk)
-        self.content = ""
-        for i, chunk in enumerate(chunks):
-            self.add_child(DocumentNode(f"{self.node_id}_c{i}", "", chunk, "", self.word_limit))
-    
     def get_node(self, node_id):
         if self.node_id == node_id:
             return self
@@ -89,8 +57,10 @@ class DocumentNode:
                 return found_node
         return None
     
-    def get_id_list(self):
+    def get_id_list(self, depth: int = 1):
         id_list = [self.node_id]
+        if depth < 0:
+            return []
         for child in self.children:
             id_list += child.get_id_list()
         return id_list
@@ -103,26 +73,36 @@ class DocumentNode:
             for child in self.children:
                 child.generate_summary()
         children_summaries = [child.summary for child in self.children]
-        if self.node_id == "references&appendix":
+        if self.node_id.startswith("references&appendix"):
             return
         print(f"Generating summary for {self.node_id}")
         self.summary = compress(self.content + "\n".join(children_summaries), compression_ratio)
         print(f"Summary length: {len(self.summary.split(' '))} words")
-        # TODO: Check if compress input is too long
     
-    def get_summary(self, depth: int = 0):
+    def get_context(self, depth: int = 0, original: bool = False):
         """
-        Get the summary of the node and its children
+        Get the context of the node and its children
         """
-        summary = ""
-        summary += self.summary + "\n"
-        if depth <= 0:
-            return summary
+        if depth < 0:
+            content = "Hidden"
+            summary = "Hidden"
+        else:
+            content = self.content if original else "Hidden"
+            summary = self.summary if not original else "Hidden"
+
+        context = {
+            "node_id": self.node_id,
+            "title": self.title,
+            "content": content,
+            "summary": summary,
+            "children": []
+        }
         for child in self.children:
-            summary += child.get_summary(depth - 1)
-        return summary
+            context["children"].append(child.get_context(depth - 1, original=False))
+        return context
 
 def extract_text_from_pdf(file_path):
+    print(f"Extracting text from {file_path}")
     with open(file_path, "rb") as file:
         pdf_reader = PyPDF2.PdfReader(file)
         text = ""
@@ -130,7 +110,7 @@ def extract_text_from_pdf(file_path):
             text += page.extract_text()
     return text
 
-def parse_nodes(file_path):
+def parse_paper(file_path):
     text = extract_text_from_pdf(file_path)
     def get_next_valid_nodes(current_node):
         next_nodes = []
@@ -191,8 +171,8 @@ def parse_nodes(file_path):
             break
     
     # Add abstract node
-    root_node = DocumentNode("root", "Root", "")
-    abstract_node = DocumentNode("abstract", "Abstract", "\n".join(lines[abstract_start + 1:abstract_end]).strip())
+    root_node = ContextNode("root", "Root", "")
+    abstract_node = ContextNode("abstract", "Abstract", "\n".join(lines[abstract_start + 1:abstract_end]).strip())
     root_node.add_child(abstract_node)
     
     for i, candidate in enumerate(node_candidates):
@@ -218,7 +198,7 @@ def parse_nodes(file_path):
 
         node_title = lines[candidate["line"]].strip()
         node_content = "\n".join(lines[candidate["line"]:end_index]).strip()
-        current_node = DocumentNode(".".join(map(str, current_code)), node_title, node_content)
+        current_node = ContextNode(".".join(map(str, current_code)), node_title, node_content)
 
         # Determine the parent of the current node
         while current_parent.node_id != "root" and len(current_node.node_id) <= len(current_parent.node_id):
@@ -229,19 +209,23 @@ def parse_nodes(file_path):
         current_parent = current_node
 
     # Add references node
-    references_node = DocumentNode("references&appendix", "References", "\n".join(lines[reference_start + 1:]).strip())
+    references_node = ContextNode("references&appendix", "References", "\n".join(lines[reference_start + 1:]).strip())
     root_node.add_child(references_node)
 
     return root_node
 
+def parse_unstructured(file_path):
+    # TODO: Implement parse_unstructured to parse PDF files based on pages
+    pass
+
 def main():
     parser = argparse.ArgumentParser(description="Generate summaries from PDF files")
     parser.add_argument("-i", "--input", type=str, help="The PDF file to generate summaries from")
-    parser.add_argument("--compression-ratio", type=str, default="1/4", help="The compression ratio to use")
+    parser.add_argument("-c", "--compression-ratio", type=str, default="1/4", help="The compression ratio to use")
     parser.add_argument("-o", "--output", type=str, default="summary.json", help="The output json file")
     args = parser.parse_args()
 
-    root_node = parse_nodes(args.input)
+    root_node = parse_paper(args.input)
     root_node.generate_summary(args.compression_ratio)
     with open(args.output, "w") as file:
         file.write(root_node.to_json())
