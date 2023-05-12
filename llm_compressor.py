bba368e3c1815dcd029fb4358a204de6e58b2248
Yibo Wei <david_wyb2001@outlook.com>
2023-05-11 21:09:24 -0700

    Modify prompts and output format

--- llm_compressor.py
+++ llm_compressor.py
@@ -1,35 +1,28 @@
+import json
 from typing import Tuple
 from api import Message, send_messages
-import nltk
-from nltk import word_tokenize
-from nltk.corpus import stopwords
-from nltk.probability import FreqDist
-from collections import Counter
-
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
 
 # TODO: Use tiktoken to count the tokenized text length
 
 SYSTEM_PROMPT = """
 You will be given a long text in triple single quotes. Think about this step by step:
 - User will specify the compression ratio and maximum word count.
+- Generate a short title that describes the content of the text.
 - Generate a short summary of the text that is compressed to the specified compression ratio and 
 maximum word count.
-- The compressed text should maintain the original meaning, tense, tone, and structure.
-- The compressed text should be one paragraph.
-- Directly output the compressed text in triple single quotes.
+- The summary must maintain the original meaning, tense, tone, and structure.
+- The summary should be one paragraph.
+- You MUST output your title and summary in JSON format.
 Example:
 User:
 Compression ratio: 1/4
 Maximum word count: 150
 Text: '''Example text'''
 Assistant:
-'''Example compressed text'''
+{"summary": "Example summary", "title": "Example title"}
 """
 
-def compress(text, compression_ratio: str = "1/4", max_words: int = "150"):
+def compress(text, compression_ratio: str = "1/4", max_words: int = "150") -> Tuple[str, str]:
     # Divide text into chunks if it is longer than 2000 words
     if len(text.split(" ")) >= 2000:
         chunks = []
@@ -56,26 +49,12 @@ def compress(text, compression_ratio: str = "1/4", max_words: int = "150"):
     ]
     messages = system_messages + user_messages
     response_message = send_messages(messages)
-    if "'''" not in response_message.content:
-        return response_message.content
-    compressed_text = response_message.content.split("'''")[1]
-    return compressed_text
-
-def generate_title_and_summary(text: str, compression_ratio: str = "1/4", max_words: int = 150) -> Tuple[str, str]:
-    # Generate the summary
-    summary = compress(text, compression_ratio, max_words)
-
-    # Tokenize the text
-    tokens = word_tokenize(text.lower())
-    # Remove stopwords and punctuation
-    stop_words = set(stopwords.words("english"))
-    filtered_tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
-
-    # Calculate word frequency and choose the top 3 keywords
-    word_freq = FreqDist(filtered_tokens)
-    top_keywords = [word for word, freq in word_freq.most_common(3)]
-
-    # Generate the title
-    title = f"{' '.join(top_keywords).title()}"
-
+    if "{" not in response_message.content or "}" not in response_message.content:
+        summary = response_message.content
+        title = ""
+    else:
+        # Find the first { and last } to get the JSON
+        response_json = json.loads(response_message.content[response_message.content.find("{"):response_message.content.rfind("}")+1])
+        summary = response_json["summary"]
+        title = response_json["title"]
     return title, summary
