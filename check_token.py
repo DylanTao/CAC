67a60dc34d73559188e597b2f11541e8324b0487
dylantao <dylantaosirui@outlook.com>
2023-05-14 23:00:09 -0700

    tokenize implemented & tested

--- check_token.py
+++ check_token.py
@@ -1,19 +1,18 @@
-from tiktoken import Tokenizer, TokenCount
+import tiktoken
 from typing import Tuple
 
-def check_token_length(input_text: str, max_length: int) -> Tuple[bool, int]:
+def check_token_length(input_text: str, max_token_length: int, model: str = "gpt-3.5-turbo") -> Tuple[bool, int]:
     # Initialize the tiktoken tokenizer
-    tokenizer = Tokenizer()
-    token_count = TokenCount()
+    encoding = tiktoken.get_encoding("cl100k_base")
+    encoding = tiktoken.encoding_for_model(model)
 
     # Tokenize the input text and count the tokens
-    for token in tokenizer.tokenize(input_text):
-        token_count.add(token)
+    token = encoding.encode(input_text)
 
     # Get the tokenized length
-    tokenized_length = token_count.total()
+    tokenized_length = len(token)
 
     # Check if the tokenized length exceeds the maximum length
-    if_exceed = tokenized_length > max_length
+    if_valid = tokenized_length > max_token_length
 
-    return if_exceed, tokenized_length
+    return if_valid, tokenized_length
