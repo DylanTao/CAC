456a30e9def6c6e6628ac79b7332528e725aa649
dylantao <dylantaosirui@outlook.com>
2023-05-16 01:33:55 -0700

    add encoder nonjson function

--- app.py
+++ app.py
@@ -2,27 +2,73 @@ import streamlit as st
 import json
 import clipboard
 from chat import ContextChatBot, SYSTEM_PROMPT
-from encoder import ContextNode
+from encoder import ContextNode, parse_unstructured, extract_text_from_pdf
+import os
 
 # Create an instance of the chatbot
 chatbot = ContextChatBot(ContextNode("root"))
 
 # Create a title and a subtitle
-tabs = st.tabs(["Context Tree", "Chat", "History", "Clipboard Chat"])
+encoder_tab, context_tree_tab, chat_tab, history_tab = st.tabs(["Customize Encoder","Context Tree", "Chat", "History"])
 
 # Side bar for file upload
-with st.sidebar:
-    files = st.file_uploader("Upload json files", type=["json"], accept_multiple_files=True)
-    if files:
-        if len(files) == 1:
-            chatbot.root_node = ContextNode.from_dict(json.load(files[0]))
-            chatbot.root_node.node_id = "root"
-        else:
-            for file in files:
-                data = json.load(file)
-                chatbot.root_node.add_child(ContextNode.from_dict(data))
+def handle_json(file):
+    data = json.load(file)
+    return ContextNode.from_dict(data)
 
-with tabs[0]:
+def handle_nonjson(file):
+    if file.type == "text/plain":
+        # copy txt to a temporary file
+        file_name = file.name
+        with open(file_name, "w") as f:
+            f.write(file.getvalue().decode("utf-8"))
+        root_node = parse_unstructured(file_name)
+    else:  # assume file is a PDF
+        # copy pdf to a temporary file
+        file_name = file.name
+        with open(file_name, "wb") as f:
+            f.write(file.getbuffer())
+        root_node = parse_unstructured(file_name)
+    root_node.apply_word_limit()
+    root_node.generate_summary(True, title=True)
+    # delete temporary file
+    os.remove(file_name)
+
+    # save the root node to a json file
+    with open(file_name + ".json", "w") as f:
+        f.write(root_node.to_json())
+
+    return root_node
+
+with st.sidebar:
+    files = st.file_uploader("Upload files", type=["json", "pdf", "txt"], accept_multiple_files=True)
+    if files:
+        for file in files:
+            if file.type == "application/json":
+                node = handle_json(file)
+            elif file.type == "text/plain":
+                node = handle_nonjson(file)
+            else:  # assume file is a PDF
+                node = handle_nonjson(file)
+            
+            if len(files) == 1:
+                chatbot.root_node = node
+                chatbot.root_node.node_id = "root"
+            else:
+                chatbot.root_node.add_child(node)
+                
+with encoder_tab:
+    # Allow user to change root node id and title
+    root_id = st.text_input("Document type", chatbot.root_node.node_id)
+    root_title = st.text_input("Default prompt", chatbot.root_node.title)
+    # if st.button("Update root node"):
+    #     chatbot.root_node.node_id = root_id
+    #     chatbot.root_node.title = root_title
+    # # Download current context tree
+    # st.download_button("Download this context tree", json.dumps(chatbot.root_node.to_dict(), indent=4), chatbot.root_node.node_id + ".json", "text/json")
+    # st.json(chatbot.root_node.to_dict())
+
+with context_tree_tab:
     # Allow user to change root node id and title
     root_id = st.text_input("Root node id", chatbot.root_node.node_id)
     root_title = st.text_input("Root node title", chatbot.root_node.title)
@@ -34,7 +80,7 @@ with tabs[0]:
     st.json(chatbot.root_node.to_dict())
 
 history = []
-with tabs[1]:
+with chat_tab:
     user_input = st.text_area("Question", "", height=200)
     submit = st.button("Ask Chatbot")
     if user_input.strip() != "" and submit:
@@ -56,6 +102,6 @@ with tabs[1]:
                 if node is not None:
                     st.json(node.get_context(1))
             
-with tabs[2]:
+with history_tab:
     for message in history:
         st.markdown(message)
